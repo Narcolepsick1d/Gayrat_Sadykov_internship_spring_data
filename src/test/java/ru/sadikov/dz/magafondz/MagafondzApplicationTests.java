0647a88f2ae667680cbf4f61503c0167c236e6c4
Sadikov Garik <sadikovgarik@gmail.com>
2022-07-21 17:28:33 +0500

    Исправил Тест для БД

--- src/test/java/ru/sadikov/dz/magafondz/MagafondzApplicationTests.java
+++ src/test/java/ru/sadikov/dz/magafondz/MagafondzApplicationTests.java
@@ -1,19 +1,25 @@
 package ru.sadikov.dz.magafondz;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 
 @SpringBootTest
 class MagafondzApplicationTests {
-
+	@Value("${spring.datasource.url}")
+	String jdbcUrl;
+	@Value("${spring.datasource.username}")
+	String user;
+	@Value("${spring.datasource.password}")
+	String pass ;
 	@Test
 	void testDBConnection() {
-		String jdbcUrl="jdbc:postgresql://localhost:5432/gayrat_internship";
-		String user= "postgres";
-		String pass = "12345";
+
+
 
 		try {
 			System.out.println("Connection to database:"+jdbcUrl);
