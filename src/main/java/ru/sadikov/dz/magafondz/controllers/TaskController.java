53f15bb32211e7dae882fe216d05fdde9275a308
Sadikov Garik <sadikovgarik@gmail.com>
2022-08-14 14:11:45 +0500

    +mongodb

--- src/main/java/ru/sadikov/dz/magafondz/controllers/TaskController.java
+++ src/main/java/ru/sadikov/dz/magafondz/controllers/TaskController.java
@@ -1,66 +1,66 @@
 package ru.sadikov.dz.magafondz.controllers;
 
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import ru.sadikov.dz.magafondz.Services.TaskEmployeeService;
-import ru.sadikov.dz.magafondz.models.TaskEmployee;
+import ru.sadikov.dz.magafondz.Reprository.TasksService;
+import ru.sadikov.dz.magafondz.models.Tasks;
 
 import java.util.List;
 
 @Controller
-@CrossOrigin(origins = "http://localhost:8080")
-@RequestMapping("/taskEmployee")
 public class TaskController {
     @Autowired
-    private TaskEmployeeService taskEmployeeService;
-   //логика для task контроллера с thymeleaf
+    private TasksService userService;
+
     @RequestMapping("/add")
-    public String addTaskEmp(){return "taskEmployee/add";}
-    @PostMapping("/taskEmployee")
-    public String saveTaskEmp(TaskEmployee taskEmployee){
-        System.out.println("Dannie"+taskEmployee);
-        taskEmployeeService.saveTaskEmp(taskEmployee);
-        return "redirect:/taskEmployee/getAllTaskEmp";
-    }
-    @RequestMapping("/taskEmployee")
-    public  String findallTaskEmp(Model model){
-        List ls = taskEmployeeService.getAllTasksEmp();
-        model.addAttribute("taskEmp",ls);
-        return "taskEmployee/show";
-    }
-    @GetMapping("/oneTaskEmp/{id}")
-    public String findOne(@PathVariable ("id")int id,Model model){
-        TaskEmployee taskEmployee = taskEmployeeService.findOne(id);
-        System.out.println("Zapros"+taskEmployee);
-        model.addAttribute("taskEmp",taskEmployee);
-        return "taskEmployee/edit";
-    }
-    @RequestMapping("/removeTaskEmp")
-    public String removeTaskEmp(int id){
-        System.out.println("udalit'"+id);
-        taskEmployeeService.removeTaskEmpById(id);
-        return "redirect:/taskEmployee/getAllTaskEmp";
-    }
-    @PutMapping("/updateTaskEmp/{id}")
-    public String updateTaskEmp(TaskEmployee taskEmployee){
-        taskEmployeeService.updateTaskEmp(taskEmployee);
-        return "redirect:/taskEmployee/getAllTaskEmp";
+    public String addUser() {
+        return "add";
     }
 
 
 
+    @PostMapping("/user")
+    public String saveUser(Tasks tasks) {
+        System.out.println("Добавленные данные："+tasks);
+        userService.saveTasks(tasks);
+        return "redirect:/getAll";
+    }
+
+
+    @RequestMapping("/getAll")
+    public String findallUser(Model model){
+        List ls= userService.getAllTasks();
+        model.addAttribute("users",ls);
+        return "show";
+    }
 
 
 
+    @GetMapping("/user/{id}")
+    public String findOne(@PathVariable("id") int id, Model model){
+        Tasks user = userService.findOne(id);
+        System.out.println("Запрошенные данные:"+user);
+        model.addAttribute("user",user);
+        return "edit";
+    }
 
+    @RequestMapping("/removeUserById")
+    public String removeUserById(int id) {
+        System.out.println("Номер, который нужно удалить："+id);
+        userService.removeTasksByTasksid(id);
+        return "redirect:/getAll";
+    }
 
+  
 
-
-
-
-
+    @PutMapping("/user/{id}")
+    public String updateUser(Tasks tasks) {
+        userService.updateTasks(tasks);
+        return "redirect:/getAll";
+    }
 
 }
